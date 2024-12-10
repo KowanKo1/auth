3eb54c6f72aae5363a52156b1b3f0cdd216ad166
jasonirvine76 <jason.irvine@ui.ac.id>
2024-12-10 12:48:16 +0700

    feat: add transaction response dto

--- routes/transactions.py
+++ routes/transactions.py
@@ -6,6 +6,7 @@ from sqlmodel import Session
 import os
 
 from typing import Annotated, List
+from dto import TransactionResponse
 from utils.auth import decode_access_token
 from crud import get_account_by_email
 from models import Transaction
@@ -17,7 +18,7 @@ security = HTTPBearer()
 TRANSACTION_SERVICE_URL = os.getenv("TRANSACTION_SERVICE_URL")
 AUTHENTICATION_SERVICE_URL = os.getenv("AUTHENTICATION_SERVICE_URL")
 
-@router.get("/", response_model=List[Transaction])
+@router.get("/", response_model=List[TransactionResponse])
 async def read_items(credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)], db:Session = Depends(get_db)):
     email = decode_access_token(credentials.credentials)
     account = get_account_by_email(db, email)
@@ -26,7 +27,7 @@ async def read_items(credentials: Annotated[HTTPAuthorizationCredentials, Depend
         raise HTTPException(status_code=400, detail="Account is not recognized")
 
     try:
-        response = requests.get(f"{TRANSACTION_SERVICE_URL}/api/transactions", headers={"origin":AUTHENTICATION_SERVICE_URL})
+        response = requests.get(f"{TRANSACTION_SERVICE_URL}/api/transactions", headers={"origin":AUTHENTICATION_SERVICE_URL, "email":email})
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         raise RuntimeError(f"Failed to log transaction: {e}")
